e81d17a1a34f8607084fdf045117cc67b0f3bb86
Steve Jenson <stevej@lambda.(none)>
2009-02-15 19:34:04 -0800

    testing that (car '(a b)) returns a. yay! an actual milestone.

---
+++ src/test/scala/com/saladwithsteve/lisp/LispSpec.scala
@@ -0,0 +1,27 @@
+/** Copyright 2008 Steve Jenson, Inc. */
+package com.saladwithsteve.lisp
+
+import scala.util.Sorting
+import java.io.{File, FileInputStream}
+import net.lag.configgy.Config
+import org.specs._
+
+object LispSpec extends Specification {
+
+  private def sorted[T <% Ordered[T]](list: List[T]): List[T] = {
+    val dest = list.toArray
+    Sorting.quickSort(dest)
+    dest.toList
+  }
+
+  "Lisp" should {
+    doAfter {
+    }
+
+    "Simple unsugared syntax" in {
+      "Car should return first element" in {
+        Lisp.eval(List(Car(List('a, 'b)))) mustEqual 'a
+      }
+    }
+  }
+}
