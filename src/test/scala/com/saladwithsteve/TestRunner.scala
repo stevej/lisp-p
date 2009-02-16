732312d67e190f783405721c1939ec483c71a050
Steve Jenson <stevej@lambda.(none)>
2009-02-15 19:33:26 -0800

    basic test infrastructure.

---
+++ src/test/scala/com/saladwithsteve/TestRunner.scala
@@ -0,0 +1,15 @@
+/** Copyright 2008 Twitter, Inc. */
+package com.saladwithsteve
+
+import net.lag.configgy.Configgy
+import net.lag.logging.Logger
+
+
+object TestRunner extends FilterableSpecsFileRunner("src/test/scala/**/*.scala") {
+//  Configgy.configure("src/resources/test.conf")
+  if (System.getProperty("debugtrace") == null) {
+    Logger.get("").setLevel(Logger.FATAL)
+  } else {
+    Logger.get("").setLevel(Logger.TRACE)
+  }
+}
