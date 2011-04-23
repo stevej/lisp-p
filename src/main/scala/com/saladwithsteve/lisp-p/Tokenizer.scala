bf3e79dd27563267277b55dd0908708e7ac04194
Steve Jenson <stevej@fruitless.org>
2011-04-23 11:56:36 -0700

    hiding punctuation behind a Map.

--- src/main/scala/com/saladwithsteve/lisp-p/Tokenizer.scala
+++ src/main/scala/com/saladwithsteve/lisp-p/Tokenizer.scala
@@ -30,28 +30,24 @@ class Tokenizer {
     }
   }
 
-  def punctuation(c: Char): Boolean = c match {
-    case '\n' => true
-    case '\t' => true
-    case ' '  => true
-    case '('  => true
-    case ')'  => true
-    case _    => false
-  }
 
-  def makePunctuation(c: Char): Token = c match {
-    case '\n' => Space
-    case '\t' => Space
-    case ' '  => Space
-    case '('  => LParen
-    case ')'  => RParen
-  }
+  val punctuation: Map[Char, Token] = Map(
+    '\n' -> Space,
+    '\t' -> Space,
+    ' '  -> Space,
+    ')'  -> RParen,
+    '('  -> LParen
+  )
+
+  def isPunctuation(c: Char): Boolean = punctuation.contains(c)
+
+  def makePunctuation(c: Char): Token = punctuation(c)
 
   /**
    * Returns the next Word and the remaining Stream
    */
   def readWord(input: Stream[Char]): (Word, Stream[Char]) = {
-    val punc = { c: Char => !punctuation(c) }
+    val punc = { c: Char => !isPunctuation(c) }
     // It seems silly that I have to walk over the stream twice.
     // FIXME: switch this to an Iteratee.
     val name = (input takeWhile punc).mkString
@@ -65,7 +61,7 @@ class Tokenizer {
 
     val tokenStream: Option[Stream[Token]] = maybeFirst.map { char =>
       char match {
-        case s if !punctuation(s) => {
+        case s if !isPunctuation(s) => {
           val (word, stream) = readWord(input)
 
           Stream.cons(word, charStreamToTokenStream(stream))
